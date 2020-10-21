cef48916a0dbd53757bfa8f87bea8056469e123b
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-21 19:53:56 +0200

    Check if data has been updated today

--- src/main/scala/com/github/frankivo/CovidStats.scala
+++ src/main/scala/com/github/frankivo/CovidStats.scala
@@ -1,5 +1,7 @@
 package com.github.frankivo
 
+import java.time.LocalDate
+
 import play.api.libs.json.{JsArray, JsString, JsValue, Json}
 import scalaj.http.Http
 
@@ -7,14 +9,25 @@ object CovidStats {
   def getData(): Unit = {
     val data = download
     val json = toJson(data)
-    val nl = filterCountry(json)
 
+    println(updatedToday(json))
+    
+    val nl = filterCountry(json)
     println(nl.length)
   }
 
   def download: String = Http("https://api.covid19api.com/dayone/country/netherlands").asString.body
 
   def toJson(raw: String): JsValue = Json.parse(raw)
+
+  def yesterday: String = s"${LocalDate.now.minusDays(1).toString}T00:00:00Z"
+
+  def updatedToday(json: JsValue): Boolean = {
+    json
+      .as[JsArray]
+      .value
+      .exists(j => (j \ "Date").as[JsString].value == yesterday)
+  }
 
   def filterCountry(json: JsValue): Seq[JsValue] = {
     json
