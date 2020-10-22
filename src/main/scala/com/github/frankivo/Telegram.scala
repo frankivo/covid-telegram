37adb4a3c85a66871ef036ac286cd73e76c2ada3
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-22 12:20:46 +0200

    Handle commands

--- src/main/scala/com/github/frankivo/Telegram.scala
+++ src/main/scala/com/github/frankivo/Telegram.scala
@@ -1,6 +1,6 @@
 package com.github.frankivo
 
-import com.pengrad.telegrambot.model.Update
+import com.pengrad.telegrambot.model.{MessageEntity, Update}
 import com.pengrad.telegrambot.request.SendMessage
 import com.pengrad.telegrambot.{TelegramBot, UpdatesListener}
 
@@ -12,8 +12,21 @@ class Telegram {
   bot.setUpdatesListener(updates => handleUpdates(updates.asScala.toSeq))
 
   private def handleUpdates(updates: Seq[Update]): Int = {
-    println(updates)
+    val commands = updates
+      .filter(u => u.message.entities != null)
+      .filter(u => u.message.entities.exists(e => e.`type`.eq(MessageEntity.Type.bot_command)))
+      .map(u => u.message.text)
+    handleCommands(commands)
+
     UpdatesListener.CONFIRMED_UPDATES_ALL
+  }
+
+  private def handleCommands(commands: Seq[String]): Unit = {
+    commands
+      .foreach {
+        case "/hi" => sendMessage("hi!")
+        case e => sendMessage(s"Unknown command: ${e}")
+      }
   }
 
   def chatId: Long = sys.env("TELEGRAM_CHATID").toLong
