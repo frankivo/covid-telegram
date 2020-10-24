356d0df08a41a8650cf354872e09993e7aeace2f
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-24 13:45:57 +0200

    Use Seq as storage

--- src/main/scala/com/github/frankivo/CovidStats.scala
+++ src/main/scala/com/github/frankivo/CovidStats.scala
@@ -15,7 +15,7 @@ case class UpdateAll()
 
 class CovidStats extends Actor {
 
-  val db: Database = new Database
+  private var stats: Seq[CovidRecord] = Seq()
 
   def download: JsValue = {
     val data = Http("https://api.covid19api.com/dayone/country/netherlands").asString.body
@@ -37,8 +37,7 @@ class CovidStats extends Actor {
       parsed.get
     }
 
-    val data = db.getDayCount(parsedDate)
-
+    val data = stats.find(r => r.date.isEqual(parsedDate))
     if (data.isEmpty) return TelegramMessage(s"No data found for ${parsedDate}")
 
     val rec = data.get
@@ -49,12 +48,10 @@ class CovidStats extends Actor {
     val json = download
     val filtered = filterCountry(json)
     val mapped = filtered.map(j => CovidRecord(getDate(j \ "Date"), getLong(j \ "Confirmed")))
-    val counts = mapped.getDailyCounts
 
-    db.clearDaily()
-    db.insertDailyCounts(counts: _*)
+    stats = mapped.getDailyCounts
 
-    TelegramMessage("Got %s records!".format(mapped.length))
+    TelegramMessage("Got %s records!".format(stats.length))
   }
 
   def getDate(field: JsLookupResult): LocalDate = LocalDate.parse(field.as[JsString].value.substring(0, 10))
