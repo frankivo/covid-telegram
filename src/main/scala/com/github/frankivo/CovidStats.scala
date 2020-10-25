a175b8219c81eb45b0e47fd896a965b70046d004
Frank Oosterhuis <frank+github@scriptzone.nl>
2020-10-25 21:34:45 +0100

    Limit update to 15 minutes

--- src/main/scala/com/github/frankivo/CovidStats.scala
+++ src/main/scala/com/github/frankivo/CovidStats.scala
@@ -3,17 +3,16 @@ package com.github.frankivo
 import java.time.LocalDate
 
 import akka.actor.Actor
-import play.api.libs.json._
 
 import scala.util.Try
 
-case class NewStats(stats: Seq[CovidRecord])
+case class Statistics(data: Seq[CovidRecord])
 
 case class GetCasesForDay(chatId: Long, date: Option[String] = None)
 
 class CovidStats extends Actor {
 
-  private var stats: Seq[CovidRecord] = Seq()
+  private var stats: Statistics = _
 
   def getDayCount(date: Option[String]): String = {
     val parsedDate = if (date.isEmpty) LocalDate.now().minusDays(1) else {
@@ -22,7 +21,7 @@ class CovidStats extends Actor {
       parsed.get
     }
 
-    val data = stats.find(r => r.date.isEqual(parsedDate))
+    val data = stats.data.find(r => r.date.isEqual(parsedDate))
     if (data.isEmpty) return s"No data found for ${parsedDate}"
 
     s"Cases for ${data.head.date}: ${data.head.count}"
@@ -30,6 +29,6 @@ class CovidStats extends Actor {
 
   override def receive: Receive = {
     case e: GetCasesForDay => sender() ! TelegramMessage(getDayCount(e.date), e.chatId)
-    case e: NewStats => stats = e.stats
+    case e: Statistics => stats = e
   }
 }
--- src/main/scala/com/github/frankivo/Updater.scala
+++ src/main/scala/com/github/frankivo/Updater.scala
@@ -1,6 +1,7 @@
 package com.github.frankivo
 
-import java.time.LocalDate
+import java.time.temporal.ChronoUnit.{MINUTES, SECONDS}
+import java.time.{LocalDate, LocalTime}
 
 import akka.actor.{Actor, ActorRef}
 import com.github.frankivo.CovidRecordHelper.CovidSequence
@@ -10,27 +11,51 @@ import scalaj.http.Http
 case class UpdateAll(chatId: Long)
 
 class Updater(stats: ActorRef) extends Actor {
+
+  var lastUpdated: LocalTime = LocalTime.MIN
+  val MIN_AGE: Int = 15
+
   override def receive: Receive = {
-    case u: UpdateAll => sender() ! TelegramMessage(backFill(), u.chatId)
+    case u: UpdateAll => sender() ! TelegramMessage(refresh(), u.chatId)
   }
 
-  def backFill(): String = {
+  private def refresh(): String = {
+    if (LocalTime.now().isBefore(nextAllowedUpdate)) s"Refresh is not allowed for another ${countDown()}"
+    else backFill()
+  }
+
+  private def nextAllowedUpdate : LocalTime = lastUpdated.plusMinutes(MIN_AGE)
+
+  private def countDown() : String = {
+    val now = LocalTime.now()
+
+    val diffMinutes = MINUTES.between(now, nextAllowedUpdate)
+    val diffSeconds = SECONDS.between(now, nextAllowedUpdate)
+    if (diffMinutes > 0) s"$diffMinutes minutes"
+    else s"$diffSeconds seconds"
+  }
+
+  private def resetTime() : Unit = lastUpdated = LocalTime.now()
+
+  private def backFill(): String = {
     val json = download
     val filtered = filterCountry(json)
     val mapped = filtered.map(j => CovidRecord(getDate(j \ "Date"), getLong(j \ "Confirmed")))
 
     val update = mapped.getDailyCounts
-    stats ! NewStats(update)
+    stats ! Statistics(update)
+
+    resetTime()
 
     "Got %s records!".format(update.length)
   }
 
-  def download: JsValue = {
+  private def download: JsValue = {
     val data = Http("https://api.covid19api.com/dayone/country/netherlands").asString.body
     Json.parse(data)
   }
 
-  def filterCountry(json: JsValue): Seq[JsValue] = {
+  private def filterCountry(json: JsValue): Seq[JsValue] = {
     json
       .as[JsArray]
       .value
@@ -38,7 +63,7 @@ class Updater(stats: ActorRef) extends Actor {
       .toSeq
   }
 
-  def getDate(field: JsLookupResult): LocalDate = LocalDate.parse(field.as[JsString].value.substring(0, 10))
+  private def getDate(field: JsLookupResult): LocalDate = LocalDate.parse(field.as[JsString].value.substring(0, 10))
 
-  def getLong(field: JsLookupResult): Long = field.as[JsNumber].value.toLong
+  private def getLong(field: JsLookupResult): Long = field.as[JsNumber].value.toLong
 }
