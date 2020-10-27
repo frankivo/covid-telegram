5bd00d96bc4a5987870673a6c468f6f1f1dca31a
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-27 14:18:24 +0100

    Fix mutable state

--- src/main/scala/com/github/frankivo/CovidStats.scala
+++ src/main/scala/com/github/frankivo/CovidStats.scala
@@ -6,24 +6,35 @@ import akka.actor.Actor
 
 import scala.util.Try
 
-case class Statistics(data: Seq[CovidRecord])
+case class Statistics(data: Seq[CovidRecord]) {
+  def findDayCount(date: LocalDate): Option[CovidRecord] = data.find(_.date.isEqual(date))
+
+  def findMaxDate(): LocalDate = data.map(_.date).max
+}
 
 case class GetCasesForDay(destination: Long, date: Option[String] = None)
 
 class CovidStats() extends Actor {
 
-  private var stats: Statistics = _
+  override def receive: Receive = onMessage(null)
 
-  def updateStats(data: Statistics): Unit = {
-    val isFirstRun = stats == null
-
-    stats = data
-    graphMonths(isFirstRun)
-
-    broadcastToday()
+  private def onMessage(stats: Statistics): Receive = {
+    case e: GetCasesForDay => CovidBot.ACTOR_TELEGRAM ! TelegramMessage(e.destination, getDayCount(stats, e.date))
+    case e: Statistics => updateStats(stats, e)
   }
 
-  def graphMonths(isFirstRun: Boolean): Unit = {
+  def updateStats(stats: Statistics, newStats: Statistics): Unit = {
+    val isFirstRun = stats == null
+
+    context.become(onMessage(newStats))
+
+    if (newStats != null) {
+      graphMonths(newStats, isFirstRun)
+      broadcastToday(newStats)
+    }
+  }
+
+  def graphMonths(stats: Statistics, isFirstRun: Boolean): Unit = {
     val grouped = stats
       .data
       .groupBy(r => (r.date.getYear, r.date.getMonthValue))
@@ -36,35 +47,27 @@ class CovidStats() extends Actor {
     }
   }
 
-  def findDayCount(date: LocalDate): Option[CovidRecord] = stats.data.find(_.date.isEqual(date))
-
-  def findMaxDate(): LocalDate = stats.data.map(_.date).max
-
-  def getDayCount(date: Option[String]): String = {
+  def getDayCount(stats: Statistics, date: Option[String]): String = {
     if (stats == null) return "Data has not been pulled yet."
 
     val parsedDate =
-      if (date.isEmpty) findMaxDate()
-    else
-    {
-      val parsed = Try(LocalDate.parse(date.get)).toOption
-      if (parsed.isEmpty) return s"Cannot parse date '${date.get}'"
-      parsed.get
-    }
+      if (date.isEmpty) stats.findMaxDate()
+      else {
+        val parsed = Try(LocalDate.parse(date.get)).toOption
+        if (parsed.isEmpty) return s"Cannot parse date '${date.get}'"
+        parsed.get
+      }
 
-    val data = findDayCount(parsedDate)
+    val data = stats.findDayCount(parsedDate)
     if (data.isEmpty) return s"No data found for $parsedDate"
 
     s"Cases for ${data.head.date}: ${data.head.count}"
   }
 
-  override def receive: Receive = {
-    case e: GetCasesForDay => CovidBot.ACTOR_TELEGRAM ! TelegramMessage(e.destination, getDayCount(e.date))
-    case e: Statistics => updateStats(e)
-  }
-
-  def broadcastToday(): Unit = {
-    findDayCount(LocalDate.now())
+  def broadcastToday(stats: Statistics): Unit = {
+    stats
+      .findDayCount(LocalDate.now())
       .foreach(r => CovidBot.ACTOR_TELEGRAM ! TelegramMessage(Telegram.broadcastId, s"There are ${r.count} new cases!"))
   }
+
 }
