654aaa073214348b6e869a3d9f5a97ffa6eaecde
Frank Oosterhuis <frank+github@scriptzone.nl>
2020-10-27 02:01:49 +0100

    Fix csv stuff

--- src/main/scala/com/github/frankivo/CsvReader.scala
+++ src/main/scala/com/github/frankivo/CsvReader.scala
@@ -1,5 +1,24 @@
 package com.github.frankivo
 
-object CsvReader {
+import java.io.{BufferedReader, File}
+import java.time.LocalDate
 
+import scala.io.{BufferedSource, Source}
+import scala.util.Try
+
+object CsvReader {
+  def readFile(file: File): CovidRecord = readFile(Source.fromFile(file))
+
+  def readFile(source: BufferedSource): CovidRecord = {
+    val rec = source
+      .getLines()
+      .toSeq
+      .map(_.split(","))
+      .filter(r => r(1).contains("Totaal"))
+      .map(r => CovidRecord(LocalDate.parse(r(0)), Try(r(2).toLong).getOrElse(0)))
+      .head
+
+    source.close
+    rec
+  }
 }
--- src/test/scala/com/github/frankivo/CvsReaderTest.scala
+++ src/test/scala/com/github/frankivo/CvsReaderTest.scala
@@ -1,5 +1,27 @@
 package com.github.frankivo
 
-class CvsReaderTest {
+import java.time.LocalDate
 
+import utest.{ArrowAssert, TestSuite, Tests, test}
+
+import scala.io.Source
+
+object CvsReaderTest extends TestSuite {
+  val tests: Tests = Tests {
+    test("read file with total") {
+      val file = Source.fromResource("RIVM_NL_national_20200421.csv")
+      val actual = CsvReader.readFile(file)
+
+      val expected = CovidRecord(LocalDate.parse("2020-04-21"), 729)
+      actual ==> expected
+    }
+
+    test("read file without total") {
+      val file = Source.fromResource("RIVM_NL_national_20200227.csv")
+      val actual = CsvReader.readFile(file)
+
+      val expected = CovidRecord(LocalDate.parse("2020-02-27"), 0)
+      actual ==> expected
+    }
+  }
 }
