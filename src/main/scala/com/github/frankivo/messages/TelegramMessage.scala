b4d93b32dbe802e33bcd32e97c53b2241ec9b382
Frank Oosterhuis <frank+github@scriptzone.nl>
2020-10-28 23:05:32 +0100

    Add comment

--- src/main/scala/com/github/frankivo/messages/TelegramMessage.scala
+++ src/main/scala/com/github/frankivo/messages/TelegramMessage.scala
@@ -1,3 +1,8 @@
 package com.github.frankivo.messages
 
+/**
+ * Send a text message on Telegram.
+ * @param destination The recipient.
+ * @param body The message.
+ */
 case class TelegramMessage(destination: Long, body: String)
