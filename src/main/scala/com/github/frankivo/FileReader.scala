946e91bf96080faa8d1db9346abd7fef5bb46d20
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2020-10-31 16:57:54 +0100

    Add comments

--- src/main/scala/com/github/frankivo/FileReader.scala
+++ src/main/scala/com/github/frankivo/FileReader.scala
@@ -8,12 +8,25 @@ import com.github.tototoshi.csv.CSVReader
 
 import scala.io.BufferedSource
 
+/**
+ * Reads file using CSVReader.
+ */
 object FileReader {
+  /**
+   * Read daily data from a BufferedSource.
+   * @param source Data source.
+   * @return DayRecord with covid data.
+   */
   def readDay(source: BufferedSource): DayRecord = readDay(CSVReader.open(source))
 
+  /**
+   * Read daily data from a File.
+   * @param file Data file.
+   * @return DayRecord with covid data.
+   */
   def readDay(file: File): DayRecord = readDay(CSVReader.open(file))
 
-  def readDay(reader: CSVReader): DayRecord = {
+  private def readDay(reader: CSVReader): DayRecord = {
     val record = reader
       .allWithHeaders()
       .filter(row => row("Type") == "Totaal")
