70e06460612e3ecf6758c8a76d462947014a58e1
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-04-23 12:29:21 +0200

    Tests

---
+++ src/test/scala/com/github/frankivo/util/DateHelperTest.scala
@@ -0,0 +1,23 @@
+package com.github.frankivo.util
+
+import com.github.frankivo.util.DateHelper.LocalDateExtender
+import utest.{ArrowAssert, TestSuite, Tests, test}
+
+import java.time.LocalDate
+
+object DateHelperTest extends TestSuite {
+
+  val tests: Tests = Tests {
+    test("week 53") {
+      LocalDate.parse("2020-12-31").weekNumber ==> 53
+    }
+
+    test("week 0") {
+      LocalDate.parse("2021-01-01").weekNumber ==> 0
+    }
+
+    test("week 1") {
+      LocalDate.parse("2021-01-04").weekNumber ==> 1
+    }
+  }
+}
